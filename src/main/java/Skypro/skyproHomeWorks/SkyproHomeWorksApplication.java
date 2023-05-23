33e8d87ee9e1ae8eb3ad3d5c778da3181707eafe
Vasilii Ostrovskii <Nezero@mail.ru>
2023-05-23 12:50:33 +0700

    Initial commit

---
+++ src/main/java/Skypro/skyproHomeWorks/SkyproHomeWorksApplication.java
@@ -0,0 +1,13 @@
+package Skypro.skyproHomeWorks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproHomeWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproHomeWorksApplication.class, args);
+	}
+
+}
